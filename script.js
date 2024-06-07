6b4cb2bfc89f5b37958f7193dcb50e6d9e566001
s19835 <s19835@sci.pdn.ac.lk>
2024-06-07 20:43:25 +0530

    line segments added

--- script.js
+++ script.js
@@ -7,3 +7,27 @@ context.lineWidth = 1;
 context.strokeStyle = `282c33`;
 context.fillStyle = `282c33`;
 
+let parameters = {a:2, b:-2, c:1, d:-1};
+
+for (var particle = 0; particle < 200; particle++) {
+    let x = Math.floor(Math.random() * width);
+    let y = Math.floor(Math.random() * height);
+
+    context.fillRect(x, y, 1, 1);
+    draw(x, y, parameters, context);
+}
+
+function draw(x, y, parameters, context) {
+
+    let ox = x;
+    x += Math.sin(parameters.a * y) + (parameters.c * Math.cos(parameters.a * x));
+    y += Math.sin(parameters.b * ox) + (parameters.d * Math.cos(parameters.b * y));
+
+    if (x > width) x = 0;
+    if (y > height) y = 0;
+    if (x < 0) x = width;
+    if (y < 0) y = height;
+
+    context.lineTo(x, y);
+    context.stroke();
+}
