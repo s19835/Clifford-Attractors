623fd9333611889398e8df79b9625c9279e3f8d3
s19835 <s19835@sci.pdn.ac.lk>
2024-06-08 14:10:30 +0530

    This variation Fails

--- script.js
+++ script.js
@@ -8,13 +8,36 @@ context.strokeStyle = `282c33`;
 context.fillStyle = `282c33`;
 
 let parameters = {a:2, b:-2, c:1, d:-1};
+var numOfParticles = 20;
 
-for (var particle = 0; particle < 200; particle++) {
+// let x = Math.floor(Math.random() * width);
+// let y = Math.floor(Math.random() * height);
+
+for (var particle = 0; particle < numOfParticles; particle++) {
     let x = Math.floor(Math.random() * width);
     let y = Math.floor(Math.random() * height);
 
-    context.fillRect(x, y, 1, 1);
-    draw(x, y, parameters, context);
+
+
+    function render() {
+        context.beginPath();
+        context.moveTo(x, y);
+        let ox = x;
+        x += width * (Math.sin(parameters.a * y) + (parameters.c * Math.cos(parameters.a * x)));
+        y += height * (Math.sin(parameters.b * ox) + (parameters.d * Math.cos(parameters.b * y)));
+        context.lineTo(x, y);
+        context.stroke();
+        requestAnimationFrame(render);
+    }
+    render();
+
+    // draw(x, y, parameters, context);
+    
+
+    if (x > width) x = 0;
+    if (y > height) y = 0;
+    if (x < 0) x = width;
+    if (y < 0) y = height;
 }
 
 function draw(x, y, parameters, context) {
