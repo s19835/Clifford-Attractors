015bf450f8b43b0bfd30ed8d4ae4a5844125a2bf
s19835 <s19835@sci.pdn.ac.lk>
2024-06-08 16:11:59 +0530

    Play around with CHAOS-I

--- script.js
+++ script.js
@@ -10,47 +10,18 @@ context.fillStyle = `282c33`;
 let parameters = {a:2, b:-2, c:1, d:-1};
 var numOfParticles = 20;
 
-// let x = Math.floor(Math.random() * width);
-// let y = Math.floor(Math.random() * height);
+//CHAOSI
+//computer simulatin of a three amplifiers, 
+/*Imagine an arrangement of three amplifiers in which the first amplifier outputs a signal x that is fed to the other two. The second amplifier outputs the signal 1 - x in response to x. The third amplifier takes the two signals, x and 1 - x, as input. It generates the product, x(1 - x), of the two signals and feeds it back to the first amplifier, which also receives a control voltage, r, as input. One additional component, a device that samples its input and delivers the same voltage as output for a short time, completes the circuit; it is inserted in the output line from the first amplifier. The three-amplifier circuit does a voltage dance that becomes more hectic as the control voltage r is grad ually increased.*/
 
-for (var particle = 0; particle < numOfParticles; particle++) {
-    let x = Math.floor(Math.random() * width);
-    let y = Math.floor(Math.random() * height);
-
-
-
-    function render() {
-        context.beginPath();
-        context.moveTo(x, y);
-        let ox = x;
-        x += width * (Math.sin(parameters.a * y) + (parameters.c * Math.cos(parameters.a * x)));
-        y += height * (Math.sin(parameters.b * ox) + (parameters.d * Math.cos(parameters.b * y)));
-        context.lineTo(x, y);
-        context.stroke();
-        requestAnimationFrame(render);
-    }
-    render();
-
-    // draw(x, y, parameters, context);
-    
-
-    if (x > width) x = 0;
-    if (y > height) y = 0;
-    if (x < 0) x = width;
-    if (y < 0) y = height;
+let r = Math.random() * 2.9 - 4;
+let x = 3 * 0.002;
+for (let i = 0; i < 200; i++) {
+    x = r * x * (1-x);
+    console.log(x);
 }
-
-function draw(x, y, parameters, context) {
-
-    let ox = x;
-    x += Math.sin(parameters.a * y) + (parameters.c * Math.cos(parameters.a * x));
-    y += Math.sin(parameters.b * ox) + (parameters.d * Math.cos(parameters.b * y));
-
-    if (x > width) x = 0;
-    if (y > height) y = 0;
-    if (x < 0) x = width;
-    if (y < 0) y = height;
-
-    context.lineTo(x, y);
-    context.stroke();
+for (let j = 0; j < 300; j++) {
+    x = r * x * (1-x);
+    context.fillRect(x *1000 , height/2, 2, 2);
+    console.log(x);
 }
