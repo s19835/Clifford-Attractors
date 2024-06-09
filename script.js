d9a5ea515d43f980b6955f39de13a99a1eddf6d0
s19835 <s19835@sci.pdn.ac.lk>
2024-06-09 17:58:24 +0530

    randomamized x, y coordinates mapped to canvas coordinates

--- script.js
+++ script.js
@@ -4,28 +4,42 @@ const canvas = document.getElementById("canvas"),
     height = canvas.height = window.innerHeight;
 
 context.lineWidth = 1;
-context.strokeStyle = `282c33`;
-context.fillStyle = `282c33`;
+context.strokeStyle = '#28c33';
+context.fillStyle = '#282c33';
 
-let parameters = {a:2, b:-2, c:1, d:-1};
-var numOfParticles = 20;
+// by changing the parameters1 or others to just parameters visualize diff attractor grpahics, but only one parameters name should exist.
+let parameters1 = {a: 2, b: -2, c: 1, d: -1};
+let parameters2 = {a: -1.4, b: 1.6, c: 1, d: 0.7}
+let parameters = {a: 1.7, b: 1.7, c: 0.6, d: 1.2}
+var numOfParticles = 1000000;
 
-//CHAOSI
-//computer simulatin of a three amplifiers, 
-/*Imagine an arrangement of three amplifiers in which the first amplifier outputs a signal x that is fed to the other two. The second amplifier outputs the signal 1 - x in response to x. The third amplifier takes the two signals, x and 1 - x, as input. It generates the product, x(1 - x), of the two signals and feeds it back to the first amplifier, which also receives a control voltage, r, as input. One additional component, a device that samples its input and delivers the same voltage as output for a short time, completes the circuit; it is inserted in the output line from the first amplifier. The three-amplifier circuit does a voltage dance that becomes more hectic as the control voltage r is grad ually increased.*/
+for (let i = 0; i < numOfParticles; i++) {
+    // Map the random coordinates to the range -2.5 to 2.5
+    let x = Math.random() * 5 - 2.5;
+    let y = Math.random() * 5 - 2.5;
 
-let r = Math.random() * 2.9 - 4;
-
-let x = 3 * 0.002;
-
-for (let i = 0; i < 200; i++) {
-    x = r * x * (1-x);
-    console.log(x);
+    //console.log(x + ', ' + y);
+    clifford(x, y); // Pass the initial coordinates to the clifford function
 }
 
+function clifford(x, y) {
+    for (let j = 0; j < 100; j++) {
+        let ox = x;
+        x = (Math.sin(parameters.a * y) + (parameters.c * Math.cos(parameters.a * x)));
+        y = (Math.sin(parameters.b * ox) + (parameters.d * Math.cos(parameters.b * y)));
+    }
+    // Map the resulting coordinates back to canvas coordinates
+    let mappedX = map(x, -2.5, 2.5, 0, width);
+    let mappedY = map(y, -2.5, 2.5, height, 0);
+    particle(mappedX, mappedY);
+    //console.log(mappedX + ', ' + mappedY);
+}
 
-for (let j = 0; j < 300; j++) {
-    x = r * x * (1-x);
-    context.fillRect(x *1000 , height/2, 2, 2);
-    console.log(x);
+function particle(x, y) {
+    context.fillRect(x, y, 0.3, 0.3);
+}
+
+// Map the x, y coordiates to the canvas coordinates
+function map(value, start1, stop1, start2, stop2) {
+    return start2 + (value - start1) * (stop2 - start2) / (stop1 - start1);
 }
